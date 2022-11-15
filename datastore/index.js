210723e5134f39284089e1458a79a21f31748923
Ivan Day <ivanday@utexas.edu>
2022-11-15 13:02:00 -0600

    Complete deleting a todo

--- datastore/index.js
+++ datastore/index.js
@@ -92,16 +92,23 @@ exports.update = (id, text, callback) => {
     }
   });
 };
-
+//fs.unlink(path, callback)
+// var item = items[id];
+// delete items[id];
+// if (!item) {
+//   // report an error if item not found
+//   callback(new Error(`No item with id: ${id}`));
+// } else {
+//   callback();
+// }
 exports.delete = (id, callback) => {
-  var item = items[id];
-  delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback();
-  }
+  fs.unlink(`${exports.dataDir}/${id}.txt`, (err) => {
+    if (err) {
+      callback(new Error(`No item with id: ${id}`));
+    } else {
+      callback();
+    }
+  });
 };
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
